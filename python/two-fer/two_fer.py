efec9c41c9aba0c90a148e8f05d941263cf6139b
Michael Bardou <michaelbardou@macbook-air-de-michael.home>
2021-12-18 21:13:05 +0100

    Add two fer python exercism

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,10 +1,10 @@
-def two_fer(name = None):
+def two_fer(name = "you"):
     '''
     ::param name if he is not null or equal to "" the function return a string with the param.
     If name is null the function return a standard string.
     '''
     
-    if name != None:
+    if name != "you":
         return "One for " + name + ", one for me."
     else:
         return "One for you, one for me."
