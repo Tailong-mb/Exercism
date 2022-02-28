a9022973e0ff99b1a15b815d1555bfb0668bc98b
Michael Bardou <michael.bardou@icloud.com>
2022-02-28 11:13:08 +0100

    add ellens-alien-game py exercism

---
+++ python/ellens-alien-game/classes.py
@@ -0,0 +1,25 @@
+class Alien:
+
+    total_aliens_created = 0
+    
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.health = 3
+        Alien.total_aliens_created += 1
+
+    def is_alive(self):
+        return self.health > 0
+
+    def hit(self):
+        self.health -= 1
+
+    def teleport(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+
+    def collision_detection(self, other_object):
+        pass
+
+def new_aliens_collection(position):
+    return [Alien(x, y) for (x, y) in position]
