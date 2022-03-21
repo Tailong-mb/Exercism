678b9e23a866dae4308409a4b85052c35fb6f3b5
Michael Bardou <michael.bardou@icloud.com>
2022-03-21 21:00:00 +0100

    add resistor color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const colorCodes = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+}
+
+export const colorCode = key => colorCodes[key];
+export const COLORS = Object.keys(colorCodes);
