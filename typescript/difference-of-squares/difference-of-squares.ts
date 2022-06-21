5c6e0cba413aa786a74ac0904fb90ef229fd5faf
Michael Bardou <michael.bardou@icloud.com>
2022-06-21 23:03:33 +0200

    add square ts

---
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -0,0 +1,20 @@
+export class Squares {
+
+  count :number
+  
+  constructor(count: number) {
+    this.count = count;
+  }
+
+  get sumOfSquares(): number {
+    return (this.count * (this.count + 1) * (2 * this.count + 1)) / 6;
+  }
+
+  get squareOfSum(): number {
+    return (this.count * (this.count +1)/2)**2
+  }
+
+  get difference(): number {
+    return this.squareOfSum - this.sumOfSquares;
+  }
+}
