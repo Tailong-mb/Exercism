2c39f1ee255c35e3f6aa238a35f9732dfafb8b5a
Michael Bardou <michael.bardou@icloud.com>
2022-07-12 10:42:07 +0200

    add factory-sensors

---
+++ javascript/factory-sensors/factory-sensors.js
@@ -0,0 +1,73 @@
+// @ts-check
+
+export class ArgumentError extends Error {}
+
+export class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`);
+    this.temperature = temperature;
+  }
+}
+
+/**
+ * Check if the humidity level is not too high.
+ *
+ * @param {number} humidityPercentage
+ * @throws {Error}
+ */
+export function checkHumidityLevel(humidityPercentage) {
+  if (humidityPercentage > 70) {
+    throw new Error("Implement the checkHumidity function");
+  }
+}
+
+/**
+ * Check if the temperature is not too high.
+ *
+ * @param {number|null} temperature
+ * @throws {ArgumentError|OverheatingError}
+ */
+export function reportOverheating(temperature) {
+    if (temperature === null) {
+      throw new ArgumentError();
+  }
+  if (temperature > 500) {
+    throw new OverheatingError(temperature);
+  }
+}
+
+/**
+ *  Triggers the needed action depending on the result of the machine check.
+ *
+ * @param {{
+ * check: function,
+ * alertDeadSensor: function,
+ * alertOverheating: function,
+ * shutdown: function
+ * }} actions
+ * @throws {ArgumentError|OverheatingError|Error}
+ */
+export function monitorTheMachine(actions) {
+  try {
+    actions.check();
+  } catch (error) {
+    switch (error.constructor) {
+      case ArgumentError:
+        actions.alertDeadSensor();
+        return;
+      case OverheatingError: {
+        if (error.temperature > 600) {
+          actions.shutdown();
+          return;
+        }
+        if (error.temperature > 500) {
+          actions.alertOverheating();
+          return;
+        }
+      }
+      default:
+        throw error;
+    }
+  }
+}
+
